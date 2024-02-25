e58ef9c8ec261eacbd745dd887e93d3edc32d077
ramanuj-07 <ramanujchaurasia09@gmail.com>
2024-02-25 10:32:00 +0530

    started coding the script.js

---
+++ Weather App/script.js
@@ -0,0 +1,7 @@
+const apiKey = "da53c1a44ce4fbf01c6ea56ad127f491";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=newyork&appid=da53c1a44ce4fbf01c6ea56ad127f491";
+
+async function checkWeather() {
+    const response = await fetch();
+    
+}
