62c6924e20455708577ad1d78bda24cf7a9c879c
ramanuj-07 <ramanujchaurasia09@gmail.com>
2024-03-13 19:10:12 +0530

    upadated

--- Weather App/script.js
+++ Weather App/script.js
@@ -9,29 +9,34 @@ async function checkWeather(city) {
 
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     let data = await response.json();
-    console.log(data);
 
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
-    document.querySelector(".humidity").innerHTML = Math.round(data.main.humidity) + "%";
+    document.querySelector(".temp").innerHTML = data.main.temp + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 
 
     if (data.weather[0].main == "Clear") {
-        weatherIcon.src = "project/Weather App/images/clear.png";
+        weatherIcon.src = "./images/clear.png";
     }
-    if (data.weather[0].main == "Cloud") {
-        weatherIcon.src = "project/Weather App/images/clear.png";
-    } if (data.weather[0].main == "Clear") {
-        weatherIcon.src = "project/Weather App/images/clear.png";
-    } if (data.weather[0].main == "Clear") {
-        weatherIcon.src = "project/Weather App/images/clear.png";
-    } if (data.weather[0].main == "Clear") {
-        weatherIcon.src = "project/Weather App/images/clear.png";
-    } if (data.weather[0].main == "Clear") {
-        weatherIcon.src = "project/Weather App/images/clear.png";
+    else if (data.weather[0].main == "Clouds") {
+        weatherIcon.src = "./images/cloud.png";
+    }
+    else if (data.weather[0].main == "Drizzle") {
+        weatherIcon.src = "./images/drizzle.png";
+    }
+    else if (data.weather[0].main == "Rain") {
+        weatherIcon.src = "./images/rain.png";
+    }
+    else if (data.weather[0].main == "Snow") {
+        weatherIcon.src = "./images/snow.png";
+    }
+    else if (data.weather[0].main == "Mist") {
+        weatherIcon.src = "./images/mist.png";
     }
 
+    document.querySelector(".weather").style.display = "block";
+    
 }
 searchBtn.addEventListener("click", () => {
     checkWeather(searchBox.value);
