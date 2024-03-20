3b7fe1b3456be28b608f571c7415b0dc4b4813a7
ramanuj-07 <ramanujchaurasia09@gmail.com>
2024-03-20 13:24:29 +0530

    working on todo list

---
+++ To-do-list/script.js
@@ -0,0 +1,20 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    
+    if (inputBox.value === '')
+    {
+        alert("You have to Add something!");
+    } 
+    else
+    {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+}
